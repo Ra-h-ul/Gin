139960cbb8ec87fa0320f8836a003479179f1c8b
Ra-h-ul <rahulpandita25@gmail.com>
2023-06-16 16:11:39 +0530

    Delete request added

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	_ "fmt"
 	"net/http"
 	"strconv"
 
@@ -9,11 +12,11 @@ import (
 
 // creating struct
 type Student struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"LastName"`
-	Roll_no   int    `json:"roll_no"`
-	Class     int    `json:"class"`
-	Phone_No  int    `json:"Phone_No"`
+	FirstName string `json:"firstname,omitempty"`
+	LastName  string `json:"LastName,omitempty"`
+	Roll_no   int    `json:"roll_no,omitempty"`
+	Class     int    `json:"class,omitempty"`
+	Phone_No  int    `json:"Phone_No,omitempty"`
 }
 
 // declaring struct values
@@ -44,6 +47,14 @@ func add_sudent_data(c *gin.Context) {
 		return
 	}
 
+	// if roll number already exixts it gives an error
+	for _, j := range Students {
+		if new_student.Roll_no == j.Roll_no {
+			c.IndentedJSON(http.StatusConflict, gin.H{"message ": "roll number already exists"})
+			return
+		}
+	}
+
 	// add new student to the student slice
 
 	Students = append(Students, new_student)
@@ -62,6 +73,7 @@ func get_specific_student(c *gin.Context) {
 
 		if s.Roll_no == roll_no_int {
 			c.IndentedJSON(http.StatusFound, s)
+
 			return
 		}
 	}
@@ -69,11 +81,56 @@ func get_specific_student(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "student not found"})
 }
 
+// get student for delete student
+func get_student_by_rollno(roll_no int) (*Student, error) {
+
+	for i, s := range Students {
+		if s.Roll_no == roll_no {
+			return &Students[i], nil
+		}
+	}
+	return nil, errors.New(" student not found")
+}
+
+// Delete request for deleting specific student
+func delete_specific_student(c *gin.Context) {
+	// returns query and boolean value
+	roll_no, ok := c.GetQuery("id")
+
+	roll_no_conv, _ := strconv.ParseInt(roll_no, 10, 32)
+	roll_no_int := int(roll_no_conv)
+
+	if !ok {
+		fmt.Println(roll_no)
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "roll number not found"})
+		return
+	}
+
+	Std, err := get_student_by_rollno(roll_no_int)
+
+	if err != nil {
+		return
+	}
+
+	set_all_null(Std)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "student deleted"})
+
+}
+
+func set_all_null(std *Student) {
+	std.Roll_no = 0
+	std.Class = 0
+	std.FirstName = ""
+	std.LastName = ""
+	std.Phone_No = 0
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/students", get_student_data)
 	router.GET("/students/:roll_no", get_specific_student)
 	router.POST("/add", add_sudent_data)
+	router.PATCH("/delete", delete_specific_student)
 	router.Run("localhost:8080")
 
 }
